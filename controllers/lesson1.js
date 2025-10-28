75e525f1f50049c5c5fb3fdfd20712d267b23e1c
migarcos <migark@hotmail.com>
2025-10-28 17:38:10 -0500

    controller added

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,13 +1,13 @@
 const indexRoute = (req, res) => {
-    res.send("Hello, This is the .: INDEX :. router");
+    res.send("Hello, This is the .: INDEX :. router with controller");
 };
 
 const homeRoute = (req, res) => {
-    res.send("Hello, This is the .: HOME :. router");
+    res.send("Hello, This is the .: HOME :. router with controller");
 };
 
 const dataRoute = (req, res) => {
-    res.send("Hello, This is the .: DATA :. router");
+    res.send("Hello, This is the .: DATA :. router with controller");
 }
 
 module.exports = {
